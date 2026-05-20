3e1554737c6e039656f506c16446343ed288f1a9
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-05-20 08:45:12 +0530

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
-provider "aws" {
-  region = "us-east-1"
-}
+
 
 
 resource "aws_s3_bucket" "abc" {
-    bucket = "jatin12341"
+    bucket = var.bucket_name
 }
 
-
+# resource "aws_s3_bucket" "abc1" {
+#     bucket = "jatin1234112121"
+# }
